3b48d918498fc48a88a17ea83f41bd8510ee7bae
akhmedov-sudo <xsatmrich@gmail.com>
2026-03-27 17:05:15 +0500

    Add files via upload

---
+++ Boolean/main.py
@@ -0,0 +1,115 @@
+# n = 465
+
+# a = 45
+
+# nta_son = n//a
+
+# n = n%a
+
+# print("Xona: ",nta_son)
+
+# print("Qoldiq: ",n)
+
+# from math import *
+# import math
+
+
+
+# a = int(input("Soni kiriting: "))
+# b = int(input("Soni kiriting: "))
+
+
+# print("Ildiz: ",math.sqrt(a))
+# print("Faktoriali: ",factorial(b))
+# print("Ekub: ", gcd(a, b))
+
+
+# ----------------------------------------------- Boolean
+
+
+# a = int(input("Son kiriting: "))
+
+
+# b = a>0
+
+# print(b)
+
+
+
+
+# a = int(input("Son kiriting: "))
+# b = a%2==0
+
+# print(b)
+
+
+
+# a = 57
+
+# b = 48
+
+# c = a%2==0 and b%2==0
+
+# print(c)
+
+
+# a = 45
+# b = 38
+
+# c = a%2==0 or b%2==0
+
+# print(c)
+
+
+
+# a = 45
+# b = a>0
+# print(b)
+
+
+# a = 8
+
+# b = a%2==0
+
+# print(b)
+
+
+
+# a = 7
+
+# b = a%2!=0
+
+# print(b)
+
+
+
+# a = 45
+# b = 12
+
+# c = a>2 and b<3
+
+# print(c)
+
+
+# a = 45
+# b = 12
+
+# c = a>=0 or b<-2
+
+# print(c)
+
+# a = 45
+# b = 12
+# c =6878
+
+# ac = a<=b and b<=c
+
+# print(ac)
+
+
+
+
+
+
+
+
