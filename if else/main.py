5759c10d0ca7c6eea9f2ad6f407f78d534c80022
akhmedov-sudo <xsatmrich@gmail.com>
2026-03-30 17:15:53 +0500

    Add files via upload

---
+++ if else/main.py
@@ -0,0 +1,27 @@
+# # if elif else
+
+# a = int(input())
+
+# if a==0:
+#     print("Nolga teng")
+# elif a>0:
+#     print("Musbat")
+# else:
+#     print("Manfiy")
+
+
+# a = int(input())
+
+# if a%2==0:
+#     print("Juft")
+# else:
+#     print("Toq")
+
+a = int(input())
+
+if a<999 and a<1000:
+    if a%2==0 and a>=100:
+        print("1-daraja")
+elif a%2!=0 and a==0:
+    if a>=100:
+        print("2-daraja")
