cb85b5cda4a602ffc7a377664875e69ce25a3e5f
akhmedov-sudo <xsatmrich@gmail.com>
2026-04-10 19:51:35 +0800

    Add files via upload

---
+++ funksiya/main.py
@@ -0,0 +1,44 @@
+# a = 10
+# s = 10
+# k = 1
+# p = int(input())
+# while s<=200:
+#     a += a/100*p
+#     s += a
+#     k+=1
+#     print(a,s)
+# print(int(s),k)
+
+# n  = int(input())
+# k = 0
+# while n>0:
+#     x = n%10
+#     k+=x
+#     k*=10
+#     n//=10
+# k//=10
+# print(k)
+
+# -------------------------------------------------------------while
+
+# def salom():
+#     print("salomm")
+# def alik():
+#     print("alikk")
+# salom()
+# alik()
+
+
+# def x2(a):
+#     return a*2
+# b = x2(4) + 2
+# print(b)
+
+def kvadrat(a):
+    return a**2
+
+b = kvadrat(5)
+
+def kub(a):
+    return a**3
+
