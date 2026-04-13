8f7f7ec1575a66c43a00ee2b908b76ae7cefb421
akhmedov-sudo <xsatmrich@gmail.com>
2026-04-13 14:25:48 +0500

    Add files via upload

---
+++ vazifa/funksiya/main.py
@@ -0,0 +1,81 @@
+# 1
+# def kvdr(a):
+#     return a ** 2
+
+# a = 5
+# print(kvdr(a))
+
+# 2
+# def kub(a):
+#     return a ** 3
+
+# a = 5
+# print(kub(a))
+
+ # 3
+# def is_even(num):
+#     return num % 2 == 0
+# print(is_even(4))
+# print(is_even(5))
+
+# # 4
+# def yegindi(a, b):
+#     return a + b
+# a = 3
+# b = 4
+# print(yegindi(a, b))
+
+# # 5
+# def raqamlar_soni(son):
+#     return len(str(abs(son)))
+
+# # Test
+# son = 12345
+# print(raqamlar_soni(son))
+
+# 6
+# def musbat_qil(son):
+#     if son < 0:
+#         return -son
+#     else:
+#         return son
+
+# print(musbat_qil(-5))
+# print(musbat_qil(3))
+
+# 7
+# def kattasi_qaytarilsin(a, b):
+#     if a > b:
+#         return a
+#     elif b > a:
+#         return b
+#     else:
+#         return "Sonlar teng"
+    
+# print(kattasi_qaytarilsin(5, 10))
+# print(kattasi_qaytarilsin(15, 10))
+
+# 8
+# def ga_bolinami(son):
+    # return son % 5 == 0
+# print(ga_bolinami(10))  # True
+# print(ga_bolinami(12))  # False
+
+# 9
+# def teskari_son(son):
+#     return int(str(son)[::-1])
+
+# print(teskari_son(123))
+# print(teskari_son(4567))
+
+# 10
+# def faktorial(son):
+    # if son < 0:
+        # return "Manfiy son uchun faktorial yo'q"
+    # result = 1
+    # for i in range(1, son + 1):
+        # result *= i
+    # return result
+
+# print(faktorial(5))
+# print(faktorial(0))
